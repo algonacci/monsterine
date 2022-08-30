1bdb7ed1fd8c48e941158a05e02eff79e9fa00f1
algonacci <algonacci1@gmail.com>
2022-08-30 17:51:42 +0700

    learnt fetch API

--- src/App.js
+++ src/App.js
@@ -6,20 +6,37 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        { name: "Linda" },
-        { name: "Frank" },
-        { name: "Jacky" },
-        { name: "Jenny" },
-      ],
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return {
+              monsters: users,
+            };
+          },
+          () => {
+            console.log(this.state.monsters);
+          }
+        )
+      );
   }
 
   render() {
     return (
       <div className="App">
         {this.state.monsters.map((monster) => {
-          return <h1>{monster.name}</h1>;
+          return (
+            <div>
+              <h1 key={monster.id}>{monster.name}</h1>
+              <p>{monster.email}</p>
+            </div>
+          );
         })}
       </div>
     );
