7c440f6b8c59e2c7816d7ce3105d595c3aa5471e
Eric Julianto <algonacci1@gmail.com>
2023-03-26 15:59:22 +0700

    Apply CSS

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,11 +1,13 @@
 import { Component } from "react";
 
+import "./search-box.styles.css";
+
 class SearchBox extends Component {
   render() {
     return (
       <input
         type="text"
-        className={this.props.className}
+        className={`search-box ${this.props.className}`}
         typeof="search"
         placeholder={this.props.placeholder}
         onChange={this.props.onChangeHandler}
